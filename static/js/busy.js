0c81ed939e2a843a2ca91c85adaa6d12cc7938d3
lbsilver <lb_silver@hotmail.com>
2016-03-14 15:10:08 -0700

    update busy.js

--- static/js/busy.js
+++ static/js/busy.js
@@ -11,3 +11,23 @@ var daterangepicker = require('bootstrap-daterangepicker');
 window.moment = moment;
 window.daterangepicker = daterangepicker;
 
+$(function() {
+  $('input[name="daterange"]').daterangepicker(
+    {    ranges: {
+           'Today': [moment(), moment()],
+           'Tomorrow':[moment().add(1, 'days'),
+                       moment().add(1, 'days')], 
+           'This week':[moment(), moment().endOf('week')],
+           'Next week':[moment().add(7, 'days').startOf('week'),
+                        moment().add(7, 'days').endOf('week')],
+           'This month':[moment(), moment().endOf('month')],
+           'Within two weeks':[moment(),moment().add(15, 'days')]
+        }}
+       );
+    });
+
+//$("input[name='calendars']").change(function() {
+//   if ($(this).is(":checked")){
+//       alert("yay");
+//   }
+//});
